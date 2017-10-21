4b3cdb0cfd1ef625faa9d0115c9adb486b066f03
awei <274635143@qq.com>
2017-10-21 18:28:06 +0800

    baiduyun

--- client/index/components/index/index.js
+++ client/index/components/index/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import css from './index.scss'
 import {Axios} from 'Public';
 import {Route, Redirect, Switch as RouterSwitch, Link} from 'react-router-dom'
+import logo from '../../public/img/logo.png'
 
 import {Layout, Menu, Icon, Switch, Breadcrumb} from 'antd';
 
 const {SubMenu} = Menu;
-const {Header, Sider, Content} = Layout;
-import logo from '../../public/img/wdp.png'
+const { Sider, Content} = Layout;
 import Bundle from '../../bundle';
 
 import HomeController from 'bundle-loader?lazy&name=home!../home'
@@ -79,59 +79,86 @@ export default class Index extends React.Component {
 
         console.log(selectedKeys);
 
-        return sta ? <div className={css.box}><Layout>
-            <Sider
-                // breakpoint="lg"
-                // collapsedWidth="64"
-                style={{background: themeSta ? '#404040' : '#fff'}}
-                trigger={null}
-                collapsible
-                collapsed={collapsed}
-            >
-                <div className={css.logo}>
-                    <img src={logo} alt=""/>
-                    {!collapsed ? <span>React16-Koa2</span> : null}
+        // return sta ? <div className={css.box}><Layout>
+        //     <Sider
+        //         // breakpoint="lg"
+        //         // collapsedWidth="64"
+        //         style={{background: themeSta ? '#404040' : '#fff'}}
+        //         trigger={null}
+        //         collapsible
+        //         collapsed={collapsed}
+        //     >
+        //         <div className={css.logo}>
+        //             <img src={logo} alt=""/>
+        //             {!collapsed ? <span>React16-Koa2</span> : null}
+        //         </div>
+        //         <Menu style={{height: 'calc(100vh - 82px)', borderRight: 0}}
+        //               theme={themeSta ? 'dark' : 'light'}
+        //               mode="inline"
+        //               onClick={this.changeMenu}
+        //               selectedKeys={[selectedKeys]}
+        //               defaultSelectedKeys={['/']}>
+        //             <Menu.Item key="/">
+        //                 <Icon type="user"/>
+        //                 <span>数据中心</span>
+        //             </Menu.Item>
+        //             <SubMenu key="sub1" title={<span><Icon type="appstore"/><span>控制中心</span></span>}>
+        //                 <Menu.Item key="/main">控制中心</Menu.Item>
+        //                 <Menu.Item key="/about">Option 5</Menu.Item>
+        //                 <Menu.Item key="6">Option 6</Menu.Item>
+        //             </SubMenu>
+        //             <Menu.Item key="3">
+        //                 <Icon type="upload"/>
+        //                 <span>设置</span>
+        //             </Menu.Item>
+        //         </Menu>
+        //         <div className={css.change_theme}>
+        //             <Switch onChange={this.changeTheme}/> {!collapsed ? <span>Change Theme</span> : null}
+        //         </div>
+        //     </Sider>
+        //     <div className={css.box}>
+        //         <Header style={{background: '#fff', padding: 0}}>
+        //             <div className={css.change_theme_btn} onClick={this.toggle}>
+        //                 <Icon
+        //                     className="trigger"
+        //                     type={collapsed ? 'menu-unfold' : 'menu-fold'}
+        //                 />
+        //             </div>
+        //         </Header>
+        //
+        //         <Content style={{background: '#ececec', height: 'calc(100vh - 84px)'}}>
+        //             <div className={css.nav}>
+        //                 <Breadcrumb>
+        //                     {breadcrumbItems}
+        //                 </Breadcrumb>
+        //             </div>
+        //             <div className={css.content}>
+        //                 <RouterSwitch>
+        //                     <Route exact path="/" component={Home}/>
+        //                     <Route path="/main" component={Main}/>
+        //                     <Route path="/about" component={About}/>
+        //                     <Redirect to="/404"/>
+        //                 </RouterSwitch>
+        //             </div>
+        //             <div className={css.agreement}>
+        //                 Copyright © 2017 react16-koa2
+        //             </div>
+        //         </Content>
+        //     </div>
+        // </Layout></div> : null
+        return <div className={css.box}>
+            <div className={css.head}>
+                <img src={logo} alt=""/>
+            </div>
+            <div>
+                <div className={css.menu}>
+                    <ul>
+                        <li className={css.menu_active}><Link to="/">主页</Link></li>
+                        <li><Link to="/main">内容</Link></li>
+                        <li><Link to="/about">关于</Link></li>
+                    </ul>
                 </div>
-                <Menu style={{height: 'calc(100vh - 82px)', borderRight: 0}}
-                      theme={themeSta ? 'dark' : 'light'}
-                      mode="inline"
-                      onClick={this.changeMenu}
-                      selectedKeys={[selectedKeys]}
-                      defaultSelectedKeys={['/']}>
-                    <Menu.Item key="/">
-                        <Icon type="user"/>
-                        <span>数据中心</span>
-                    </Menu.Item>
-                    <SubMenu key="sub1" title={<span><Icon type="appstore"/><span>控制中心</span></span>}>
-                        <Menu.Item key="/main">控制中心</Menu.Item>
-                        <Menu.Item key="/about">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key="3">
-                        <Icon type="upload"/>
-                        <span>设置</span>
-                    </Menu.Item>
-                </Menu>
-                <div className={css.change_theme}>
-                    <Switch onChange={this.changeTheme}/> {!collapsed ? <span>Change Theme</span> : null}
-                </div>
-            </Sider>
-            <div className={css.box}>
-                <Header style={{background: '#fff', padding: 0}}>
-                    <div className={css.change_theme_btn} onClick={this.toggle}>
-                        <Icon
-                            className="trigger"
-                            type={collapsed ? 'menu-unfold' : 'menu-fold'}
-                        />
-                    </div>
-                </Header>
-
-                <Content style={{background: '#ececec', height: 'calc(100vh - 84px)'}}>
-                    <div className={css.nav}>
-                        <Breadcrumb>
-                            {breadcrumbItems}
-                        </Breadcrumb>
-                    </div>
+                <div className={css.content_box}>
                     <div className={css.content}>
                         <RouterSwitch>
                             <Route exact path="/" component={Home}/>
@@ -143,8 +170,9 @@ export default class Index extends React.Component {
                     <div className={css.agreement}>
                         Copyright © 2017 react16-koa2
                     </div>
-                </Content>
+                </div>
             </div>
-        </Layout></div> : null
+
+        </div>
     }
 }
--- client/index/components/login/index.js
+++ client/index/components/login/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import {Axios} from 'Public'
 import {Link} from 'react-router-dom'
-import {Form, Icon, Input, Button, Checkbox} from 'antd';
+import {Form, Icon, Input, Button, Checkbox, Carousel} from 'antd';
 import css from './login.scss'
+import bg1 from '../../public/img/bg1.jpg'
+import bg2 from '../../public/img/bg2.jpg'
+import bg3 from '../../public/img/bg3.jpg'
+import bg4 from '../../public/img/bg4.jpg'
+import leftquote from '../../public/img/leftquote.png'
+import rightquote from '../../public/img/rightquote.png'
+import logo from '../../public/img/logo.png'
 
 const FormItem = Form.Item;
 
@@ -31,38 +38,188 @@ class Index extends React.Component {
 
     render() {
         const {getFieldDecorator} = this.props.form;
-        return <div className={css.box}>
-            <h1 className={css.title}>React16-Koa2</h1>
-            <Form onSubmit={this.handleSubmit}>
-                <FormItem>
-                    {getFieldDecorator('userName', {
-                        rules: [{required: true, message: '请输入用户名!'}],
-                    })(
-                        <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="用户名"/>
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: [{required: true, message: '请输入密码!'}],
-                    })(
-                        <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
-                               placeholder="密码"/>
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                    })(
-                        <Checkbox>记住我</Checkbox>
-                    )}
-                    <a href="">忘记密码</a>
-                    <Button type="primary" htmlType="submit">
-                        登录
-                    </Button>
-                    或 <Link to="/register">去注册</Link>
-                </FormItem>
-            </Form>
+        // return <div className={css.box}>
+        //     <h1 className={css.title}>React16-Koa2</h1>
+        //     <Form onSubmit={this.handleSubmit}>
+        //         <FormItem>
+        //             {getFieldDecorator('userName', {
+        //                 rules: [{required: true, message: '请输入用户名!'}],
+        //             })(
+        //                 <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="用户名"/>
+        //             )}
+        //         </FormItem>
+        //         <FormItem>
+        //             {getFieldDecorator('password', {
+        //                 rules: [{required: true, message: '请输入密码!'}],
+        //             })(
+        //                 <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
+        //                        placeholder="密码"/>
+        //             )}
+        //         </FormItem>
+        //         <FormItem>
+        //             {getFieldDecorator('remember', {
+        //                 valuePropName: 'checked',
+        //                 initialValue: true,
+        //             })(
+        //                 <Checkbox>记住我</Checkbox>
+        //             )}
+        //             <a href="">忘记密码</a>
+        //             <Button type="primary" htmlType="submit">
+        //                 登录
+        //             </Button>
+        //             或 <Link to="/register">去注册</Link>
+        //         </FormItem>
+        //     </Form>
+        // </div>
+        return <div className={css.boxs}>
+            <div className={css.login_logo}>
+                <div>
+                    <img src={logo} alt=""/>
+                </div>
+                <div className={css.login_manu}>
+                    <ul>
+                        <li className={css.txt_danger}>百度网盘严打违规文件和盗版侵权传播</li>
+                        <li>百度首页</li>
+                        <li>客户端下载</li>
+                        <li>官方贴吧</li>
+                        <li>官方微博</li>
+                        <li>问题反馈</li>
+                        <li className={css.vip}>会员中心</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <Carousel autoplay effect="fade">
+                    <div className={css.car_list}>
+                        <img src={bg1} alt=""/>
+                        <div className={css.car_item}>
+                            <div>
+                                <div className={css.car_l}>
+                                    <img src={leftquote} alt=""/>
+                                </div>
+                                <div className={css.car_c}>安全存储</div>
+                                <div className={css.car_r}>
+                                    &nbsp;
+                                </div>
+                            </div>
+                            <div>
+                                <div className={css.car_l}>
+                                    &nbsp;
+                                </div>
+                                <div className={css.car_c}>生活井井有条</div>
+                                <div className={css.car_r}>
+                                    <img src={rightquote} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={css.car_list}>
+                        <img src={bg2} alt=""/>
+                        <div className={css.car_item}>
+                            <div>
+                                <div className={css.car_l}>
+                                    <img src={leftquote} alt=""/>
+                                </div>
+                                <div className={css.car_c}>在线预览</div>
+                                <div className={css.car_r}>
+                                    &nbsp;
+                                </div>
+                            </div>
+                            <div>
+                                <div className={css.car_l}>
+                                    &nbsp;
+                                </div>
+                                <div className={css.car_c}>文件即开即看</div>
+                                <div className={css.car_r}>
+                                    <img src={rightquote} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={css.car_list}>
+                        <img src={bg3} alt=""/>
+                        <div className={css.car_item}>
+                            <div>
+                                <div className={css.car_l}>
+                                    <img src={leftquote} alt=""/>
+                                </div>
+                                <div className={css.car_c}>多端并用</div>
+                                <div className={css.car_r}>
+                                    &nbsp;
+                                </div>
+                            </div>
+                            <div>
+                                <div className={css.car_l}>
+                                    &nbsp;
+                                </div>
+                                <div className={css.car_c}>数据随身携带</div>
+                                <div className={css.car_r}>
+                                    <img src={rightquote} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={css.car_list}>
+                        <img src={bg4} alt=""/>
+                        <div className={css.car_item}>
+                            <div>
+                                <div className={css.car_l}>
+                                    <img src={leftquote} alt=""/>
+                                </div>
+                                <div className={css.car_c}>好友分享</div>
+                                <div className={css.car_r}>
+                                    &nbsp;
+                                </div>
+                            </div>
+                            <div>
+                                <div className={css.car_l}>
+                                    &nbsp;
+                                </div>
+                                <div className={css.car_c}>共度幸福时光</div>
+                                <div className={css.car_r}>
+                                    <img src={rightquote} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Carousel>
+            </div>
+            <div className={css.box}>
+                <h2 className={css.title}>账号密码登录</h2>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [{required: true, message: '请输入用户名!'}],
+                        })(
+                            <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="用户名"/>
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{required: true, message: '请输入密码!'}],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
+                                   placeholder="密码"/>
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(
+                            <Checkbox>记住我</Checkbox>
+                        )}
+                        <a href="">忘记密码</a>
+                        <Button type="primary" htmlType="submit">
+                            登录
+                        </Button>
+                        或 <Link to="/register">去注册</Link>
+                    </FormItem>
+                </Form>
+            </div>
+            <div className={css.agreement}>
+                Copyright © 2017 react16-koa2
+            </div>
         </div>
     }
 }
