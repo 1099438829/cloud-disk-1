de19ef7ebb1be32f627748c159283d0294f07407
awei <274635143@qq.com>
2017-11-07 20:02:01 +0800

    config

--- server/config.js
+++ server/config.js
@@ -6,11 +6,11 @@ module.exports = {
         path: '/',
         httpOnly: false
     },
-    url: 'http://localhost:3000', //开发
-    // url: 'https://serve.bstu.cn', // 线上
-    img_path: 'C:/Users/ccnn/Desktop/examples/react16-koa2/server/public/img', //开发
+    // url: 'http://localhost:3000', //开发
+    url: 'https://serve.bstu.cn', // 线上
+    // img_path: 'C:/Users/ccnn/Desktop/examples/react16-koa2/server/public/img', //开发
     // img_path: '/Users/awei/app/react16-koa2/server/public/img', //开发
-    // img_path: '/data/wwwroot/react16-koa2/server/public/img', // 线上
+    img_path: '/data/wwwroot/react16-koa2/server/public/img', // 线上
     path_num: 58, //开发
     // path_num: 50, //开发
     // path_num: 50, //线上
